128064847431ebac0d0e4e9aab2f55f471bd8ef9
Gireesh G <137136620+ggireeshbe@users.noreply.github.com>
2023-07-16 01:29:27 +0530

    Add files via upload

---
+++ Python/Activities/Activity15.py
@@ -0,0 +1,5 @@
+x=5
+try:
+    print(x)
+except NameError:
+    print("x hasn't been defined yet.")
---
+++ Python/Activities/Activity2.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number: "))
+mod = num % 2
+if mod > 0:
+    print("The Number is odd number.")
+else:
+    print("The number you entered is even")
